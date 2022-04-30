7d66280f2a8c16c05934679a06a4460fe62c74f4
Luthfi Izzuddin <luthfi.izzuddin1@gmail.com>
2022-05-01 01:16:10 +0700

    Create Header component and put to arrange LandingPage
    Also change default font family into Poppins in _variables.scss

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LandingPage from "pages/LandingPage";
+
 import "assets/scss/style.scss";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" component={LandingPage}></Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
